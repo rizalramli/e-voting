d25cb3af53a5ad70667d01ec547f85fbb0c4c39d
Mohamad Rizal Ramli <rizalramli993@gmail.com>
2020-12-21 00:01:14 +0700

    Rekapitulasi per kandidat

--- application/controllers/dashboard/RecapitulationController.php
+++ application/controllers/dashboard/RecapitulationController.php
@@ -13,6 +13,32 @@ class RecapitulationController extends CI_Controller
 
     public function index()
     {
-        $this->template->load('layouts/app', 'dashboard/recapitulation/index');
+        // Hasil rekapitulasi BEM
+        $where_election_bem   = array(
+            'voting_id'  => 1,
+        );
+
+        $data['election_grand_total_bem'] = $this->M_crud->edit_data($where_election_bem, 'view_election')->num_rows();
+        $data['result_data_bem'] = $this->M_crud->edit_data($where_election_bem, 'view_recapitulation_candidate')->result();
+        // end
+
+        // Hasil rekapitulasi BLM
+        $where_election_blm   = array(
+            'voting_id'  => 2,
+        );
+
+        $data['election_grand_total_blm'] = $this->M_crud->edit_data($where_election_blm, 'view_election')->num_rows();
+        $data['result_data_blm'] = $this->M_crud->edit_data($where_election_blm, 'view_recapitulation_candidate')->result();
+        // end
+
+        // Hasil rekapitulasi DLM
+        $where_election_dlm   = array(
+            'voting_id'  => 3,
+        );
+
+        $data['election_grand_total_dlm'] = $this->M_crud->edit_data($where_election_dlm, 'view_election')->num_rows();
+        $data['result_data_dlm'] = $this->M_crud->edit_data($where_election_dlm, 'view_recapitulation_candidate')->result();
+        // end
+        $this->template->load('layouts/app', 'dashboard/recapitulation/index', $data);
     }
 }
--- application/views/dashboard/recapitulation/index.php
+++ application/views/dashboard/recapitulation/index.php
@@ -11,8 +11,8 @@
                         <select name="pilihan" id="pilihan" class="form-control" onchange="myFunction()">
                             <option value=""> -Pilih- </option>
                             <option value="bem">BEM</option>
-                            <option value="dlm">DLM</option>
                             <option value="blm">BLM</option>
+                            <option value="dlm">DLM</option>
                         </select>
                     </div>
                 </div>
@@ -33,16 +33,15 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr class="bg-success text-white">
-                                        <td>Paslon 1</td>
-                                        <td>70</td>
-                                        <td>70%</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Paslon 2</td>
-                                        <td>30</td>
-                                        <td>30%</td>
-                                    </tr>
+                                    <?php foreach ($result_data_bem as $item_data_bem) :
+                                        $percentage_bem = ($item_data_bem->election_total * 100) / $election_grand_total_bem;
+                                    ?>
+                                        <tr>
+                                            <td><?php echo $item_data_bem->candidate_name ?></td>
+                                            <td><?php echo $item_data_bem->election_total ?></td>
+                                            <td><?php echo $percentage_bem . "%" ?></td>
+                                        </tr>
+                                    <?php endforeach; ?>
                                 </tbody>
                             </table>
                         </div>
@@ -50,58 +49,7 @@
                 </div>
             </div>
         </div>
-        <div id="dlm" class="d-none">
-            <div class="row">
-                <div class="col-md-7">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="text-center">Hasil</h5>
-                            <table width="100%" class="table table-sm table-striped dataTables">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Nama</th>
-                                        <th scope="col">Jumlah Suara</th>
-                                        <th scope="col">Persentase</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr class="bg-success text-white">
-                                        <td>Paslon 1</td>
-                                        <td>70</td>
-                                        <td>70%</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Paslon 2</td>
-                                        <td>30</td>
-                                        <td>30%</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-5">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="text-center">Hasil Partai</h5>
-                            <div class="card">
-                                <div class="row no-gutters">
-                                    <div class="col-sm-4" style="background: #868e96;">
-                                        <img width="70px" height="70x" src="<?php echo base_url('assets/photo/partai/nasdem.png') ?>" class="card-img-top h-100" alt="...">
-                                    </div>
-                                    <div class="col-sm-8">
-                                        <div class="card-body">
-                                            <h5 class="card-title">Alice Liddel</h5>
-                                            <p class="card-text">70 Suara / 70 %</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+
         <div id="blm" class="d-none">
             <div class="row">
                 <div class="col-md-7">
@@ -117,16 +65,15 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr class="bg-success text-white">
-                                        <td>Paslon 1</td>
-                                        <td>70</td>
-                                        <td>70%</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Paslon 2</td>
-                                        <td>30</td>
-                                        <td>30%</td>
-                                    </tr>
+                                    <?php foreach ($result_data_blm as $item_data_blm) :
+                                        $percentage_blm = ($item_data_blm->election_total * 100) / $election_grand_total_blm;
+                                    ?>
+                                        <tr>
+                                            <td><?php echo $item_data_blm->candidate_name ?></td>
+                                            <td><?php echo $item_data_blm->election_total ?></td>
+                                            <td><?php echo $percentage_blm . "%" ?></td>
+                                        </tr>
+                                    <?php endforeach; ?>
                                 </tbody>
                             </table>
                         </div>
@@ -140,6 +87,58 @@
                                 <div class="row no-gutters">
                                     <div class="col-sm-4" style="background: #868e96;">
                                         <img width="70px" height="70x" src="<?php echo base_url('assets/photo/partai/pdip.jpeg') ?>" class="card-img-top h-100" alt="...">
+                                    </div>
+                                    <div class="col-sm-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Alice Liddel</h5>
+                                            <p class="card-text">70 Suara / 70 %</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="dlm" class="d-none">
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="text-center">Hasil</h5>
+                            <table width="100%" class="table table-sm table-striped dataTables">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Nama</th>
+                                        <th scope="col">Jumlah Suara</th>
+                                        <th scope="col">Persentase</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($result_data_dlm as $item_data_dlm) :
+                                        $percentage_dlm = ($item_data_dlm->election_total * 100) / $election_grand_total_dlm;
+                                    ?>
+                                        <tr>
+                                            <td><?php echo $item_data_dlm->candidate_name ?></td>
+                                            <td><?php echo $item_data_dlm->election_total ?></td>
+                                            <td><?php echo $percentage_dlm . "%" ?></td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="text-center">Hasil Partai</h5>
+                            <div class="card">
+                                <div class="row no-gutters">
+                                    <div class="col-sm-4" style="background: #868e96;">
+                                        <img width="70px" height="70x" src="<?php echo base_url('assets/photo/partai/nasdem.png') ?>" class="card-img-top h-100" alt="...">
                                     </div>
                                     <div class="col-sm-8">
                                         <div class="card-body">
