be9769b506b41cdca45815ed9cde850f480d7053
palindungan <rizkika.palindungan1@gmail.com>
2020-12-20 21:36:01 +0700

    update js + css fix blocked browser
    update js + css fix blocked browser

--- application/views/layouts/css.php
+++ application/views/layouts/css.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="<?php echo base_url(); ?>assets/stisla/download_css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<link rel="stylesheet" href="<?php echo base_url(); ?>assets/stisla/download_css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/stisla/download_css/bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/stisla/download_css/all.css">
 <link href="<?php echo base_url(); ?>assets/stisla/download_css/dataTables.bootstrap4.min.css" rel="stylesheet">
 <link rel="stylesheet" href="<?php echo base_url(); ?>assets/stisla/download_css/buttons.bootstrap4.min.css">
 
