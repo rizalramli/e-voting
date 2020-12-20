f285f89475ff222a663655d844dcc83f2f24b8d1
palindungan <rizkika.palindungan1@gmail.com>
2020-12-20 23:35:14 +0700

    Update sidebar.php

--- application/views/layouts/sidebar.php
+++ application/views/layouts/sidebar.php
@@ -5,7 +5,7 @@
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Menu</li>
-            <li><a class="nav-link" href=""><i class="fas fa-pencil-ruler"></i> <span>Dashboard</span></a></li>
+            <li><a class="nav-link" href="<?php echo base_url('recapitulation') ?>"><i class="fas fa-pencil-ruler"></i> <span>Dashboard</span></a></li>
             <li class="menu-header">Master</li>
             <li class=""><a class="nav-link" href="<?php echo base_url('voting') ?>"><i class="fas fa-pencil-ruler"></i> <span>Data Acara</span></a></li>
             <li class=""><a class="nav-link" href="<?php echo base_url('party') ?>"><i class="fas fa-pencil-ruler"></i> <span>Data Partai</span></a></li>
