f73630f50cacc9d48eb3de4144e291aa4d881ea3
palindungan <rizkika.palindungan1@gmail.com>
2020-12-21 13:53:21 +0700

    sidebar active class
    asdas

--- application/views/layouts/sidebar.php
+++ application/views/layouts/sidebar.php
@@ -4,12 +4,20 @@
             <a href="index.html">E-Voting</a>
         </div>
         <ul class="sidebar-menu">
-            <li class="menu-header">Menu</li>
-            <li><a class="nav-link" href="<?php echo base_url('recapitulation') ?>"><i class="fas fa-pencil-ruler"></i> <span>Dashboard</span></a></li>
+            <li class="menu-header">Dashboard</li>
+            <li class="<?php if ($this->uri->segment(1) == 'recapitulation') {
+                            echo 'active';
+                        } ?>"><a class="nav-link" href="<?php echo base_url('recapitulation') ?>"><i class="fas fa-pencil-ruler"></i> <span>Rekapitulasi</span></a></li>
             <li class="menu-header">Master</li>
-            <li class=""><a class="nav-link" href="<?php echo base_url('voting') ?>"><i class="fas fa-pencil-ruler"></i> <span>Data Acara</span></a></li>
-            <li class=""><a class="nav-link" href="<?php echo base_url('party') ?>"><i class="fas fa-pencil-ruler"></i> <span>Data Partai</span></a></li>
-            <li class=""><a class="nav-link" href="<?php echo base_url('voter') ?>"><i class="fas fa-pencil-ruler"></i> <span>Data Pemilih</span></a></li>
+            <li class="<?php if ($this->uri->segment(1) == 'voting') {
+                            echo 'active';
+                        } ?>"><a class="nav-link" href="<?php echo base_url('voting') ?>"><i class="fas fa-pencil-ruler"></i> <span>Data Acara</span></a></li>
+            <li class="<?php if ($this->uri->segment(1) == 'party') {
+                            echo 'active';
+                        } ?>"><a class="nav-link" href="<?php echo base_url('party') ?>"><i class="fas fa-pencil-ruler"></i> <span>Data Partai</span></a></li>
+            <li class="<?php if ($this->uri->segment(1) == 'voter') {
+                            echo 'active';
+                        } ?>"><a class="nav-link" href="<?php echo base_url('voter') ?>"><i class="fas fa-pencil-ruler"></i> <span>Data Pemilih</span></a></li>
         </ul>
     </aside>
 </div>
