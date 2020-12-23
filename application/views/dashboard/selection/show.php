f4c45adc958835851b6fa96a1ee97ff7d79b36e1
palindungan <rizkika.palindungan1@gmail.com>
2020-12-23 08:41:02 +0700

    Update show.php

--- application/views/dashboard/selection/show.php
+++ application/views/dashboard/selection/show.php
@@ -18,8 +18,10 @@
                         <thead>
                             <tr>
                                 <th scope="col">No</th>
-                                <th scope="col">Nama</th>
-                                <th scope="col">Email</th>
+                                <?php if ($this->session->userdata("role") == "KPU") { ?>
+                                    <th scope="col">Nama</th>
+                                    <th scope="col">Email</th>
+                                <?php } ?>
                                 <th scope="col">Waktu</th>
                                 <th scope="col">Status</th>
                                 <?php if ($this->session->userdata("role") == "KPU") { ?>
@@ -34,8 +36,10 @@
                             ?>
                                 <tr>
                                     <td class="align-middle"><?php echo $i ?></td>
-                                    <td class="align-middle"><?php echo $item->name ?></td>
-                                    <td class="align-middle"><?php echo $item->email ?></td>
+                                    <?php if ($this->session->userdata("role") == "KPU") { ?>
+                                        <td class="align-middle"><?php echo $item->name ?></td>
+                                        <td class="align-middle"><?php echo $item->email ?></td>
+                                    <?php } ?>
                                     <td class="align-middle"><?php echo $item->create_at ?></td>
                                     <td class="align-middle">
                                         <?php if ($item->election_status == 0) {
